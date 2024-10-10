2013946e11c5682c125a45ee6e002135e8ffa30d
Pavluke <60747557+Pavluke@users.noreply.github.com>
2024-10-10 10:09:20 +0300

    refactor(gap-extensions): simplify gap extensions and remove last gap

--- lib/easy_gap.dart
+++ lib/easy_gap.dart
@@ -2,13 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 extension GapExt<T extends Widget> on List<T> {
-  List<Widget> gap(double dimension, {bool removeLast = true}) =>
-      _gapExtBuildWith(Gap(dimension));
+  List<Widget> gap(double extent) => _gapExtBuildWith(Gap(extent));
 
-  List<Widget> sliverGap(double dimension, {bool removeLast = true}) =>
-      _gapExtBuildWith(SliverGap(dimension));
+  List<Widget> sliverGap(double extent) => _gapExtBuildWith(SliverGap(extent));
 
-  List<Widget> _gapExtBuildWith(Widget gap) => isEmpty
-      ? this
-      : expand((w) => [w, if (indexOf(w) != indexOf(last)) gap]).toList();
+  List<Widget> _gapExtBuildWith(Widget gap) =>
+      isEmpty ? this : expand((widget) => [widget, gap]).toList()
+        ..removeLast();
 }
