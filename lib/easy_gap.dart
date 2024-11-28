7bf91788d463e7b5f70af27e1883933c291442dc
Pavluke <60747557+Pavluke@users.noreply.github.com>
2024-11-28 23:16:52 +0300

    add docs

--- lib/easy_gap.dart
+++ lib/easy_gap.dart
@@ -2,8 +2,12 @@ import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 extension GapExt<T extends Widget> on List<T> {
+  /// Return a widget list with Gap between that takes a fixed [extent] of space.
+  /// The [extent] must not be null and must be positive.
   List<Widget> gap(double extent) => _gapExtBuildWith(Gap(extent));
 
+  /// Return a widget list with SliverGap between that takes a fixed [extent] of space.
+  /// The [extent] must not be null and must be positive.
   List<Widget> sliverGap(double extent) => _gapExtBuildWith(SliverGap(extent));
 
   List<Widget> _gapExtBuildWith(Widget gap) =>
